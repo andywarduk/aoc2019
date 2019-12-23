5779280ba32fede5b7038f63b256e0d92c74645c
Andy <andy.ward.uk@gmail.com>
2019-12-23 13:40:03 +0000

    Better threading

--- index23-2.js
+++ index23-2.js
@@ -1,219 +1,129 @@
 const fs = require('fs')
 const path = require('path')
-const wt = require('worker_threads')
 const IntCode = require('./IntCode')
 
-if (wt.isMainThread) {
-    // Main thread
+const input = fs.readFileSync(path.join(__dirname, 'input23.txt'), {encoding: 'utf8'})
 
-    main()
+const prog = input.split(',').map(x => {
+    return parseInt(x)
+})
 
-} else {
-    // Worker
-    try {
-        const input = fs.readFileSync(path.join(__dirname, 'input23.txt'), {encoding: 'utf8'})
+run(prog)
 
-        const mem = input.split(',').map(x => {
-            return parseInt(x)
-        })
-
-        run(mem, wt.workerData)
-
-    } catch (e) {
-        wt.parentPort.postMessage({
-            type: 'error',
-            e
-        })
-
-    }
-}
-
-async function main()
+function run(prog)
 {
-    const worker = []
-    let natPacket = null
+    let natMsg = null
     let lastNatY = 0
+    const threads = []
+    let finished = false
 
-    function sendNatPacket()
-    {
-        // All idle?
-        if (natPacket != null && worker.find(w => w.queue > 0) === undefined) {
-            if (natPacket.y == lastNatY) {
-                message(lastNatY)
-                process.exit(0)
-            } else {
-                message(`Nat Y = ${natPacket.y}`)
-                lastNatY = natPacket.y
-                ++worker[0].queue
-                worker[0].worker.postMessage(natPacket)
-                natPacket = null
-            }
+    for (let i = 0; i < 50; i++) {
+        threads[i] = {
+            sentAddr: false,
+            inMsg: false,
+            mem: [...prog],
+            pc: 0,
+            queue: [],
+            outMsgElem: 0,
+            outMsg: null,
+            idleCnt: 0
         }
     }
 
-    for (let nic = 0; nic < 50; nic++) {
-        worker[nic] = {
-            worker: new wt.Worker(__filename, {
-                workerData: {
-                    nic
-                }
-            }),
-            online: false,
-            queue: 0,
-            msgQueue: []
-        }
+    while (!finished) {
+        for (let i = 0; i < 50; i++) {
+            const thread = threads[i]
 
-        worker[nic].worker.on('message', (msg) => {
-            switch (msg.type) {
-            case 'code':
-                message(`${msg.fromAddr} -> ${msg.toAddr} (x ${msg.x} y ${msg.y})`)
-                if (msg.toAddr == 255) {
-                    natPacket = msg
-                    sendNatPacket()
+            let res = IntCode.exec(false, thread.mem, () => {
+                let res
+
+                if (!thread.sentAddr) {
+                    // Send address
+                    res = i
+                    thread.sentAddr = true
+
+                } else if (thread.inMsg) {
+                    // Send Y
+                    res = thread.queue[0].y
+                    thread.queue.shift()
+                    thread.inMsg = false
+
+                } else if (thread.queue.length > 0) {
+                    // Got in message
+                    res = thread.queue[0].x
+                    thread.inMsg = true
+                    thread.idleCnt = 0
+
+                } else if (thread.idleCnt == 0) {
+                    // First idle
+                    res = -1
+                    ++thread.idleCnt
+                
                 } else {
-                    if (worker[msg.toAddr].online) {
-                        worker[msg.toAddr].worker.postMessage(msg)
-                    } else {
-                        worker[msg.toAddr].msgQueue.push(msg)
-                    }
-                    ++worker[msg.toAddr].queue
-                    message(` + ${msg.toAddr} queue ${worker[msg.toAddr].queue}`)
+                    // Second idle
+                    res = 'idle'
+                    thread.idleCnt = 0
+
                 }
-                break
-            case 'msg':
-                message(msg.msg)
-                break
-            case 'dequeue':
-                --worker[nic].queue
-                message(` - ${nic} queue ${worker[nic].queue}`)
-                sendNatPacket()
-                break
-            default:
-                message(msg)
-            }
-        })
 
-        worker[nic].worker.on('online', () => {
-            worker[nic].online = true
+                return res
 
-            if (worker[nic].msgQueue.length > 0) {
-                for (let m of worker[nic].msgQueue) {
-                    worker[nic].worker.postMessage(m)
-                }
-                worker[nic].msgQueue = []
-            }
-        })
-
-    }
-
-    await Promise.all(worker)
-}
-
-function message(msg)
-{
-    if (wt.isMainThread) {
-        console.log(msg)
-    } else {
-        wt.parentPort.postMessage({
-            type: 'msg',
-            msg
-        })
-    }
-}
-
-function run(mem, data)
-{
-    const addr = data.nic
-    let sentAddr = false
-    const msgQueue = []
-    let inMsgStage = 0
-    let outMsgElem = 0
-    let outMsg
-
-    wt.parentPort.on('message', (msg) => {
-        msgQueue.push(msg)
-    })
-
-    setImmediate(() => exec(0))
-
-    function exec(pc)
-    {
-        let res = IntCode.exec(false, mem, () => {
-            let res
-
-            if (!sentAddr) {
-                res = addr
-                sentAddr = true
-
-            } else {
-                switch (inMsgStage) {
+            }, (val) => {
+                switch (thread.outMsgElem) {
                 case 0:
-                    // Poll for in message
-                    res = 'input'
-                    inMsgStage = 1
+                    thread.outMsg = {
+                        to: val
+                    }
+                    ++thread.outMsgElem
 
                     break
 
                 case 1:
-                    // Got in message?
-                    if (msgQueue.length > 0) {
-                        // Yes - send X
-                        res = msgQueue[0].x
-                        inMsgStage = 2
-                    } else {
-                        // No - return -1 and re-poll next time
-                        res = -1
-                        inMsgStage = 0
-                    }
+                    thread.outMsg.x = val
+                    ++thread.outMsgElem
 
                     break
 
                 case 2:
-                    // Send Y
-                    res = msgQueue[0].y
-                    inMsgStage = 0
-                    msgQueue.shift()
-                    wt.parentPort.postMessage({
-                        type: 'dequeue'
-                    })
+                    thread.outMsg.y = val
+                    thread.outMsgElem = 0
+                    if (thread.outMsg.to == 255) {
+                        natMsg = thread.outMsg
+                    } else {
+                        // console.log(`${i} -> ${thread.outMsg.to}`)
+                        threads[thread.outMsg.to].queue.push(thread.outMsg)
+                    }
+
+                    break
 
                 }
 
+            }, thread.pc)
+        
+            if (res.stopReason == 'idle') {
+                thread.pc = res.lastPc
+            } else {
+                console.error(res)
+                process.exit(1)
             }
-
-            return res
-
-        }, (val) => {
-            switch (outMsgElem) {
-            case 0:
-                outMsg = {
-                    type: 'code',
-                    fromAddr: addr,
-                    toAddr: val
-                }
-                ++outMsgElem
-
-                break
-
-            case 1:
-                outMsg.x = val
-                ++outMsgElem
-
-                break
-
-            case 2:
-                outMsg.y = val
-                outMsgElem = 0
-                wt.parentPort.postMessage(outMsg)
-
-                break
-
-            }
-
-        }, pc)
-            
-        if (res.stopReason == 'input') {
-            setImmediate(() => exec(res.lastPc))
         }
+
+
+        if (threads.find(t => t.queue.length > 0) === undefined) {
+            // All idle
+            if (natMsg != null) {
+                if (lastNatY == natMsg.y) {
+                    console.log(lastNatY)
+                    finished = true
+                } else {
+                    // console.log(natMsg)
+                    lastNatY = natMsg.y
+                    threads[0].queue.push(natMsg)
+                    natMsg = null
+                }
+            }
+        }
+
     }
+
 }
